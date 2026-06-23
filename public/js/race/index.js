089ca125c80f887cb5b8d84214cb0f94dc380a00
Johan Lopez <johan@corporacionazul.com>
2026-06-23 10:34:31 -0600

    feat(navbar): add mobile menu toggle functionality (js)

---
+++ public/js/race/index.js
@@ -0,0 +1,11 @@
+/* ===============================
+   NAVBAR
+================================ */
+
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenuButton.classList.toggle('is-active');
+    navbarMenu.classList.toggle('is-active');
+});
